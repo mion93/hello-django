2f49e5ad0b2f83a71ae4da65cfe0860cc61dc25a
Miriam Gurrieri <mion93@hotmail.it>
2020-12-30 16:08:39 +0000

    deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
